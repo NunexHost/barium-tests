a37778c0c7c6abf7c7c4f3b3e0363bf5a0bf8322
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-17 11:18:43 -0300

    Update ClientWorldMixin.java

--- src/main/java/pedrixzz/barium/mixin/world/ClientWorldMixin.java
+++ src/main/java/pedrixzz/barium/mixin/world/ClientWorldMixin.java
@@ -1,10 +1,16 @@
 package pedrixzz.barium.mixin.world;
 
 import net.minecraft.client.world.ClientWorld;
-import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.world.chunk.Chunk;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+
 import java.util.List;
 
 @Mixin(ClientWorld.class)
