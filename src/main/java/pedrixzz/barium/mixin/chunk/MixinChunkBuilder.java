a3410cad808d278105a1321403ca7b79eb0228b9
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-18 11:20:20 -0300

    Test

---
+++ src/main/java/pedrixzz/barium/mixin/chunk/MixinChunkBuilder.java
@@ -0,0 +1,15 @@
+package pedrixzz.barium.mixin.chunk;
+
+import net.minecraft.client.render.chunk.ChunkBuilder;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(ChunkBuilder.class)
+public class MixinChunkBuilder {
+    @ModifyVariable(method = "<init>", index = 9, at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Lists;newArrayListWithExpectedSize(I)Ljava/util/ArrayList;", remap = false))
+    private int modifyThreadPoolSize(int prev) {
+        // Utilize uma única thread para minimizar o overhead
+        return 1;
+    }
+}
