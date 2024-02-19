9c6f05c1157e299d28375647c9dc1ada043106bb
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-19 18:31:07 -0300

    Update BlockModelRendererMixin.java

--- src/main/java/pedrixzz/barium/mixin/render/model/block/BlockModelRendererMixin.java
+++ src/main/java/pedrixzz/barium/mixin/render/model/block/BlockModelRendererMixin.java
@@ -2,7 +2,7 @@ package pedrixzz.barium.mixin.render;
 
 import net.minecraft.client.model.Model;
 import net.minecraft.client.render.block.BlockModelRenderer;
-import net.minecraft.util.math.Boxes
+import net.minecraft.util.math.Boxes;
 import net.minecraft.util.math.Box;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
