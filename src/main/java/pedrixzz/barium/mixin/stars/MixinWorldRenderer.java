a3cfc5057de99334d443cb30ca60f10b135a7768
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-19 20:28:53 -0300

    Create MixinWorldRenderer.java

---
+++ src/main/java/pedrixzz/barium/mixin/stars/MixinWorldRenderer.java
@@ -0,0 +1,23 @@
+package me.flashyreese.mods.sodiumextra.mixin.stars;
+
+import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.world.ClientWorld;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(WorldRenderer.class)
+public class MixinWorldRenderer {
+
+
+    @Redirect(
+            method = "renderSky(Lnet/minecraft/client/util/math/MatrixStack;Lorg/joml/Matrix4f;FLnet/minecraft/client/render/Camera;ZLjava/lang/Runnable;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/world/ClientWorld;method_23787(F)F"
+            )
+    )
+    public float redirectGetStarBrightness(ClientWorld instance, float f) {
+        return instance.method_23787(f) : 0.0f;
+    }
+}
