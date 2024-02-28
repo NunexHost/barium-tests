5b5b82340732aa14b960dd6ada44725b33d89822
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-02-27 22:07:56 -0300

    Update ChunkMixin.java

--- src/main/java/pedrixzz/barium/mixin/chunk/ChunkMixin.java
+++ src/main/java/pedrixzz/barium/mixin/chunk/ChunkMixin.java
@@ -1,45 +1,30 @@
 package pedrixzz.barium.mixin.chunk;
 
-import net.minecraft.block.Block;
-import net.minecraft.chunk.Chunk;
-import net.minecraft.network.packet.s2c.play.ChunkData;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Modify;
 
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentLinkedDeque;
-
-import javax.annotation.Nonnull;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.ChunkCache;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mixin(Chunk.class)
 public class ChunkMixin {
 
-    // Cache de dados de chunk
-    private ChunkData chunkData;
+    @Inject
+    private ChunkCache chunkCache;
 
-    // Pool de objetos para chunks
-    private static ObjectPool<Chunk> chunkPool = new ObjectPool<>();
-
-    @Inject(at = "ctor")
-    public void init(World world, int chunkX, int chunkZ, boolean shouldCreate) {
-        this.chunkData = new ChunkData(world, chunkX, chunkZ);
-        this.chunkPool.put(this);
+    @Modify
+    private void modifyTick(int tick) {
+        // Armazenar em cache dados frequentemente acessados.
+        chunkCache.cacheChunkData(chunk);
     }
 
-    // Método otimizado para obter um bloco
-    @Getter
-    public Block getBlock(int x, int y, int z) {
-        return this.chunkData.getBlock(x, y, z);
-    }
-
-    // Método otimizado para definir um bloco
-    public void setBlock(int x, int y, int z, Block block) {
-        this.chunkData.setBlock(x, y, z, block);
+    @Modify
+    private List<Entity> modifyEntities(List<Entity> entities) {
+        // Otimizar a iteração sobre entidades.
+        return entities.stream().filter(entity -> entity.isAlive()).collect(Collectors.toList());
     }
 
 }
