bf37c82420b2d115ff91e7942a088d89e04587e2
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-05-25 12:11:11 -0300

    Update PlayerListMixin.java

--- src/main/java/pedrixzz/barium/mixin/PlayerListMixin.java
+++ src/main/java/pedrixzz/barium/mixin/PlayerListMixin.java
@@ -2,15 +2,15 @@ package pedrixzz.barium.mixin;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.minecraft.class_310;
-import net.minecraft.class_355;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.hud.PlayerListHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Environment(EnvType.CLIENT)
 @Mixin(
-   value = {class_355.class},
+   value = {PlayerListHud.class},
    priority = 10
 )
 public abstract class PlayerListMixin {
@@ -21,7 +21,7 @@ public abstract class PlayerListMixin {
    target = "Lnet/minecraft/client/MinecraftClient;isInSingleplayer()Z"
 )
    )
-   private boolean renderHeads(class_310 instance) {
+   private boolean renderHeads(MinecraftClient instance) {
       return true;
    }
 }
