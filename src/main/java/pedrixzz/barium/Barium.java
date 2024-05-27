994c8f8471a5198447557f86ace2e010f6d39026
PedrixzZ <131414300+PedrixzZ@users.noreply.github.com>
2024-05-27 20:54:33 -0300

    Update Barium.java

--- src/main/java/pedrixzz/barium/Barium.java
+++ src/main/java/pedrixzz/barium/Barium.java
@@ -1,21 +1,33 @@
 package pedrixzz.barium;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.ArmorEquipEvent;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.entity.effect.StatusEffects;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ArmorItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 
 public class BotaLendaria implements ModInitializer {
 
-    private static final Identifier LEGENDARY_BOOT_ID = new Identifier("Barium", "Bota_Lendaria"); // Replace "examplemod" with your mod id
+    private static final Identifier LEGENDARY_BOOT_ID = new Identifier("Barium", "BotaLendaria"); // Replace "examplemod" with your mod id
 
     @Override
     public void onInitialize() {
         // Register the Legendary Boot item
         Registry.ITEM.register(LEGENDARY_BOOT_ID, new BotaLendaria(ArmorMaterial.DIAMOND, new PlayerEntityEquipmentSlot[]{EntityEquipmentSlot.FEET}, Settings.group(ItemGroup.COMBAT)));
+
+        // Register server tick event listener for speed effect
+        MinecraftForge.EVENT_BUS.addListener(ServerTickEvent.class, event -> {
+            for (PlayerEntity player : event.getServerWorld().getPlayers()) {
+                ItemStack boots = player.getInventory().getArmorStack(EntityEquipmentSlot.FEET);
+
+                if (boots.getItem().equals(BotaLendaria.LEGENDARY_BOOTS) && boots.getName().getString().equals("Bota Lendária")) {
+                    player.addStatusEffect(new StatusEffectInstance(StatusEffects.SPEED, 200, 1));
+                } else {
+                    player.removeStatusEffect(StatusEffects.SPEED);
+                }
+            }
+        });
     }
-}
+    }
